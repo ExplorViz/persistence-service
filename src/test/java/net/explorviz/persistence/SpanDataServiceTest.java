f59c8602bc0de329cf8912af1414c45efa4764a2
Philipp Wulff <stu234669@mail.uni-kiel.de>
2026-03-02 17:38:54 +0100

    Dynamic Data Test restructuring

--- src/test/java/net/explorviz/persistence/SpanDataServiceTest.java
+++ src/test/java/net/explorviz/persistence/SpanDataServiceTest.java
@@ -23,6 +23,7 @@ import net.explorviz.persistence.repository.CommitRepository;
 import net.explorviz.persistence.repository.FileRevisionRepository;
 import net.explorviz.persistence.repository.LandscapeRepository;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.neo4j.ogm.model.Result;
 import org.neo4j.ogm.session.Session;
@@ -51,41 +52,126 @@ class SpanDataServiceTest {
     session.purgeDatabase();
   }
 
-  @Test
-  void testPersistSpan() {
-    SpanData testSpanData =
-        SpanData.newBuilder()
-            .setSpanId("mySpan")
-            .setTraceId("myTrace")
-            .setApplicationName("hello-world")
-            .setLandscapeTokenId("mytokenvalue")
-            .setFunctionFqn("net.explorviz.helloworld.MyClass.myMethod")
-            .setStartTime(1)
-            .setEndTime(5)
-            .build();
+  @Nested
+  class WithoutStaticData {
 
-    Empty reply = spanDataService.persistSpan(testSpanData).await()
-        .atMost(Duration.ofSeconds(5));
-    assertNotNull(reply);
+    @Test
+    void testPersistSpan() {
+      SpanData testSpanData =
+          SpanData.newBuilder()
+              .setSpanId("mySpan")
+              .setTraceId("myTrace")
+              .setApplicationName("hello-world")
+              .setLandscapeTokenId("mytokenvalue")
+              .setFunctionFqn("net.explorviz.helloworld.MyClass.myMethod")
+              .setStartTime(1)
+              .setEndTime(5)
+              .build();
 
-    Session session = sessionFactory.openSession();
+      Empty reply = spanDataService.persistSpan(testSpanData).await()
+          .atMost(Duration.ofSeconds(5));
+      assertNotNull(reply);
 
-    Application result = session.queryForObject(Application.class, """
-        MATCH (app:Application {name: 'hello-world'})
-              -[:HAS_ROOT]->(:Directory)
-              -[:CONTAINS]->(:Directory {name: 'net'})
-              -[:CONTAINS]->(:Directory {name: 'explorviz'})
-              -[:CONTAINS]->(:Directory {name: 'helloworld'})
-              -[:CONTAINS]->(:FileRevision {name: 'MyClass'})
-              -[:CONTAINS]->(:Function {name: 'myMethod'})
-              <-[:REPRESENTS]-(:Span {spanId: 'mySpan'})
-              <-[:CONTAINS]-(:Trace {traceId: 'myTrace'})
-              <-[:CONTAINS]-(:Landscape {tokenId: 'mytokenvalue'})
-        RETURN app;""", Map.of());
+      Session session = sessionFactory.openSession();
 
-    assertNotNull(result);
+      Application result = session.queryForObject(Application.class, """
+          MATCH (app:Application {name: 'hello-world'})
+                -[:HAS_ROOT]->(:Directory)
+                -[:CONTAINS]->(:Directory {name: 'net'})
+                -[:CONTAINS]->(:Directory {name: 'explorviz'})
+                -[:CONTAINS]->(:Directory {name: 'helloworld'})
+                -[:CONTAINS]->(:FileRevision {name: 'MyClass'})
+                -[:CONTAINS]->(:Function {name: 'myMethod'})
+                <-[:REPRESENTS]-(:Span {spanId: 'mySpan'})
+                <-[:CONTAINS]-(:Trace {traceId: 'myTrace'})
+                <-[:CONTAINS]-(:Landscape {tokenId: 'mytokenvalue'})
+          RETURN app;""", Map.of());
+
+      assertNotNull(result);
+    }
+
+    /**
+     * If a commit hash is included in the span, the corresponding node should be connected /
+     * created.
+     */
+    @Test
+    void testPersistSpanWithCommit() {
+      SpanData testSpanData =
+          SpanData.newBuilder()
+              .setSpanId("mySpan")
+              .setTraceId("myTrace")
+              .setApplicationName("hello-world")
+              .setLandscapeTokenId("mytokenvalue")
+              .setFunctionFqn("net.explorviz.helloworld.MyClass.myMethod")
+              .setStartTime(1)
+              .setEndTime(5)
+              .setCommitId("commit1")
+              .build();
+
+      Empty reply = spanDataService.persistSpan(testSpanData).await()
+          .atMost(Duration.ofSeconds(5));
+      assertNotNull(reply);
+
+      Session session = sessionFactory.openSession();
+
+      Commit commit = session.queryForObject(Commit.class, """
+          MATCH (:Application {name: 'hello-world'})
+                -[:HAS_ROOT]->(:Directory)
+                -[:CONTAINS]->(:Directory {name: 'net'})
+                -[:CONTAINS]->(:Directory {name: 'explorviz'})
+                -[:CONTAINS]->(:Directory {name: 'helloworld'})
+                -[:CONTAINS]->(file:FileRevision {name: 'MyClass'})
+                -[:CONTAINS]->(:Function {name: 'myMethod'})
+                <-[:REPRESENTS]-(:Span {spanId: 'mySpan'})
+                <-[:CONTAINS]-(:Trace {traceId: 'myTrace'})
+                <-[:CONTAINS]-(:Landscape {tokenId: 'mytokenvalue'})
+          MATCH (commit:Commit {hash: 'commit1'})-[:CONTAINS]->(file)
+          RETURN commit;""", Map.of());
+
+      assertNotNull(commit);
+    }
+
+    /**
+     * Persisting the same span twice should create no additional nodes.
+     */
+    @Test
+    void testPersistSpanIdempotent() {
+      SpanData testSpanData =
+          SpanData.newBuilder()
+              .setSpanId("mySpan")
+              .setTraceId("myTrace")
+              .setApplicationName("hello-world")
+              .setLandscapeTokenId("mytokenvalue")
+              .setFunctionFqn("net.explorviz.helloworld.MyClass.myMethod")
+              .setStartTime(1)
+              .setEndTime(5)
+              .build();
+
+      Empty reply = spanDataService.persistSpan(testSpanData).await()
+          .atMost(Duration.ofSeconds(5));
+      assertNotNull(reply);
+
+      reply = spanDataService.persistSpan(testSpanData).await()
+          .atMost(Duration.ofSeconds(5));
+      assertNotNull(reply);
+
+      Session session = sessionFactory.openSession();
+
+      Result result = session.query("""
+          RETURN
+            COUNT {(:FileRevision)} AS files,
+            COUNT {(:Application)} AS apps,
+            COUNT {(:Span)} AS spans,
+            COUNT {(:Trace)} AS traces""", Map.of());
+
+      Map<String, Object> countMap = result.queryResults().iterator().next();
+      assertEquals(1, (Long) countMap.get("files"));
+      assertEquals(1, (Long) countMap.get("apps"));
+      assertEquals(1, (Long) countMap.get("spans"));
+      assertEquals(1, (Long) countMap.get("traces"));
+    }
+
   }
-
   /**
    * If a commit hash is included in the span and the corresponding commit, file and function nodes
    * exist, then the span should be connected to the existing function node.
@@ -151,46 +237,9 @@ class SpanDataServiceTest {
         RETURN commit;""", Map.of());
 
     assertNotNull(foundCommit);
+
+  @Nested
+  class WithStaticData {
+
   }
-
-  /**
-   * Persisting the same span twice should create no additional nodes.
-   */
-  @Test
-  void testPersistSpanIdempotent() {
-    SpanData testSpanData =
-        SpanData.newBuilder()
-            .setSpanId("mySpan")
-            .setTraceId("myTrace")
-            .setApplicationName("hello-world")
-            .setLandscapeTokenId("mytokenvalue")
-            .setFunctionFqn("net.explorviz.helloworld.MyClass.myMethod")
-            .setStartTime(1)
-            .setEndTime(5)
-            .build();
-
-    Empty reply = spanDataService.persistSpan(testSpanData).await()
-        .atMost(Duration.ofSeconds(5));
-    assertNotNull(reply);
-
-    reply = spanDataService.persistSpan(testSpanData).await()
-        .atMost(Duration.ofSeconds(5));
-    assertNotNull(reply);
-
-    Session session = sessionFactory.openSession();
-
-    Result result = session.query("""
-        RETURN
-          COUNT {(:FileRevision)} AS files,
-          COUNT {(:Application)} AS apps,
-          COUNT {(:Span)} AS spans,
-          COUNT {(:Trace)} AS traces""", Map.of());
-
-    Map<String, Object> countMap = result.queryResults().iterator().next();
-    assertEquals(1, (Long) countMap.get("files"));
-    assertEquals(1, (Long) countMap.get("apps"));
-    assertEquals(1, (Long) countMap.get("spans"));
-    assertEquals(1, (Long) countMap.get("traces"));
-  }
-
 }
