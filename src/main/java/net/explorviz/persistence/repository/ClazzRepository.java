190517b1a12db9fc18547c3a52efa071da12dd43
Philipp Wulff <stu234669@mail.uni-kiel.de>
2026-03-15 13:54:14 +0100

    Resolved MR threads

--- src/main/java/net/explorviz/persistence/repository/ClazzRepository.java
+++ src/main/java/net/explorviz/persistence/repository/ClazzRepository.java
@@ -17,72 +17,90 @@ import org.neo4j.ogm.session.SessionFactory;
 @SuppressWarnings({"PMD.AvoidDuplicateLiterals", "PMD.UseObjectForClearerAPI"})
 public class ClazzRepository {
 
-  @Inject
-  private SessionFactory sessionFactory;
+  @Inject private SessionFactory sessionFactory;
 
-  public Optional<Clazz> findClassDynamicData(final Session session, final String landscapeToken,
-      final String applicationName, final String[] filePathSegments,
+  public Optional<Clazz> findClassDynamicData(
+      final Session session,
+      final String landscapeToken,
+      final String applicationName,
+      final String[] filePathSegments,
       final String classNameSegments) {
-    return Optional.ofNullable(session.queryForObject(Clazz.class, """
-        MATCH (:Landscape {tokenId: $tokenId})--*(app:Application {name: $appName})
-        MATCH (app)-[:HAS_ROOT]->(appRoot:Directory)
-        MATCH p = (pathRoot:Directory|FileRevision)-[:CONTAINS]->*(file:FileRevision)
-        WHERE
-          (appRoot)-[:CONTAINS]->(pathRoot) AND
-          NOT (:Commit)-[:CONTAINS]->(file)) AND
-          all(j IN range(0, length(p)) WHERE nodes(p)[j].name = $filePathSegments[j]) AND
-          size(nodes(p)) = size($filePathSegments)
-        MATCH p = (:Clazz)-[:CONTAINS]->*(class:Clazz)
-        WHERE
-          (file)-[:CONTAINS]->(class) AND
-          all(j IN range(0, length(p)) WHERE nodes(p)[j].name = $classNameSegments[j]) AND
-          size(nodes(p)) = size($classNameSegments)
-        OPTIONAL MATCH (class)-[r]->(other)
-        RETURN DISTINCT class, r, other;
-        """, Map.of("tokenId", landscapeToken, "appName", applicationName, "filePathSegments",
-        filePathSegments, "classNameSegments", classNameSegments)));
+    return Optional.ofNullable(
+        session.queryForObject(
+            Clazz.class,
+            """
+            MATCH (:Landscape {tokenId: $tokenId})--*(app:Application {name: $appName})
+            MATCH (app)-[:HAS_ROOT]->(appRoot:Directory)
+            MATCH p = (pathRoot:Directory|FileRevision)-[:CONTAINS]->*(file:FileRevision)
+            WHERE
+              (appRoot)-[:CONTAINS]->(pathRoot) AND
+              NOT (:Commit)-[:CONTAINS]->(file)) AND
+              all(j IN range(0, length(p)) WHERE nodes(p)[j].name = $filePathSegments[j]) AND
+              size(nodes(p)) = size($filePathSegments)
+            MATCH p = (:Clazz)-[:CONTAINS]->*(class:Clazz)
+            WHERE
+              (file)-[:CONTAINS]->(class) AND
+              all(j IN range(0, length(p)) WHERE nodes(p)[j].name = $classNameSegments[j]) AND
+              size(nodes(p)) = size($classNameSegments)
+            OPTIONAL MATCH (class)-[r]->(other)
+            RETURN DISTINCT class, r, other;
+            """,
+            Map.of(
+                "tokenId",
+                landscapeToken,
+                "appName",
+                applicationName,
+                "filePathSegments",
+                filePathSegments,
+                "classNameSegments",
+                classNameSegments)));
   }
 
-  public Optional<Clazz> findClassByClassPathAndFileRevisionId(final Session session,
-      final String[] classPath, final Long fileRevisionId) {
-    return Optional.ofNullable(session.queryForObject(Clazz.class, """
-        MATCH (file:FileRevision)
-        WHERE id(file) = $fileId
-        
-        MATCH p = (file)-[:CONTAINS]->(:Clazz)
-                  -[:CONTAINS]->*(:Clazz)
-        WHERE
-        all(j IN range(0, length(p)) WHERE nodes(p)[j+1].name = $pathSegments[j]) AND
-        size(nodes(p))-1 = size($pathSegments)
-        
-        RETURN last(nodes(p));
-        """, Map.of("pathSegments", classPath, "fileId", fileRevisionId)));
+  public Optional<Clazz> findClassByClassPathAndFileRevisionId(
+      final Session session, final String[] classPath, final Long fileRevisionId) {
+    return Optional.ofNullable(
+        session.queryForObject(
+            Clazz.class,
+            """
+            MATCH (file:FileRevision)
+            WHERE id(file) = $fileId
+
+            MATCH p = (file)-[:CONTAINS]->*(:Clazz)
+            WHERE
+            all(j IN range(0, length(p)) WHERE nodes(p)[j+1].name = $pathSegments[j]) AND
+            size(nodes(p))-1 = size($pathSegments)
+
+            RETURN last(nodes(p));
+            """,
+            Map.of("pathSegments", classPath, "fileId", fileRevisionId)));
   }
 
   public Optional<Map<String, Object>> findLongestMatchingClassPathByFileRevisionsId(
       final Session session, final String[] classPath, final Long fileRevisionId) {
-    final Result result = session.query("""
-        MATCH (file:FileRevision)
-        WHERE id(file) = $fileId
-        
-        OPTIONAL MATCH p = (file)-[:CONTAINS]->(:Clazz)
-          -[:CONTAINS]->*(:Clazz)
-        
-        WITH p, nodes(p)[1..] AS classes
-        WHERE classes IS NULL
-          OR  all(i IN range(0, size(classes)-1)
-            WHERE classes[i].name = $pathSegments[i])
-        
-        ORDER BY size(classes) DESC
-        LIMIT 1
-        
-        RETURN
-        CASE
-          WHEN classes IS NULL or size(classes) = 0 THEN null
-          ELSE classes[-1]
-        END AS existingClass,
-        $pathSegments[coalesce(size(classes),0)..] AS remainingPath
-        """, Map.of("pathSegments", classPath, "fileId", fileRevisionId));
+    final Result result =
+        session.query(
+            """
+            MATCH (file:FileRevision)
+            WHERE id(file) = $fileId
+
+            OPTIONAL MATCH p = (file)-[:CONTAINS]->*(:Clazz)
+
+            WITH p, nodes(p)[1..] AS classes
+            WHERE classes IS NULL
+              OR  all(i IN range(0, size(classes)-1)
+                WHERE classes[i].name = $pathSegments[i])
+
+            ORDER BY size(classes) DESC
+            LIMIT 1
+
+            RETURN
+            CASE
+              WHEN classes IS NULL or size(classes) = 0 THEN null
+              ELSE classes[-1]
+            END AS existingClass,
+            $pathSegments[coalesce(size(classes),0)..] AS remainingPath
+            """,
+            Map.of("pathSegments", classPath, "fileId", fileRevisionId));
 
     final Iterator<Map<String, Object>> resultIterator = result.queryResults().iterator();
     if (!resultIterator.hasNext()) {
@@ -93,16 +111,16 @@ public class ClazzRepository {
   }
 
   /*
-   The fileId is a fallback. If findLongestMatchingClassPathByFileRevisionsId doesn't find any
-   existing clazz, then the whole clazz path will be created and the first one will be added to
-   the corresponding FileRevision
-   */
+  The fileId is a fallback. If findLongestMatchingClassPathByFileRevisionsId doesn't find any
+  existing clazz, then the whole clazz path will be created and the first one will be added to
+  the corresponding FileRevision
+  */
   @SuppressWarnings("PMD.NullAssignment")
-  public Clazz createClazzPathAndReturnLastClazz(final Session session, final String[] classPath,
-      final Long fileRevisionId) {
+  public Clazz createClazzPathAndReturnLastClazz(
+      final Session session, final String[] classPath, final Long fileRevisionId) {
     final Map<String, Object> resultMap =
-        findLongestMatchingClassPathByFileRevisionsId(session, classPath, fileRevisionId).orElse(
-            null);
+        findLongestMatchingClassPathByFileRevisionsId(session, classPath, fileRevisionId)
+            .orElse(null);
 
     Clazz existingClazz = null;
 
@@ -112,11 +130,15 @@ public class ClazzRepository {
     final String[] remainingPath;
 
     if (existingClazz == null) {
-      final FileRevision fileRevision = session.queryForObject(FileRevision.class, """
-          MATCH (f:FileRevision)
-          WHERE id(f)=$fileId
-          RETURN f;
-          """, Map.of("fileId", fileRevisionId));
+      final FileRevision fileRevision =
+          session.queryForObject(
+              FileRevision.class,
+              """
+              MATCH (f:FileRevision)
+              WHERE id(f)=$fileId
+              RETURN f;
+              """,
+              Map.of("fileId", fileRevisionId));
       existingClazz = new Clazz(classPath[0]);
       fileRevision.addClass(existingClazz);
       session.save(fileRevision);
@@ -137,22 +159,40 @@ public class ClazzRepository {
   }
 
   public Optional<Clazz> findClassByLandscapeTokenAndRepositoryAndFileHashAndClazzName(
-      final Session session, final String tokenId, final String repoName, final String fileHash,
+      final Session session,
+      final String tokenId,
+      final String repoName,
+      final String fileHash,
       final String clazzName) {
-    return Optional.ofNullable(session.queryForObject(Clazz.class, """
-        MATCH (:Landscape {tokenId: $tokenId})
-          -[:CONTAINS]->(:Repository {name: $repoName})
-          -[:CONTAINS]->(:Commit)
-          -[:CONTAINS]->(f:FileRevision {hash: $fileHash})
-          -[:CONTAINS]->(cl:Clazz {name: $clazzName})
-        RETURN cl
-        LIMIT 1;
-        """, Map.of("tokenId", tokenId, "repoName", repoName, "fileHash", fileHash, "clazzName",
-        clazzName)));
+    return Optional.ofNullable(
+        session.queryForObject(
+            Clazz.class,
+            """
+            MATCH (:Landscape {tokenId: $tokenId})
+              -[:CONTAINS]->(:Repository {name: $repoName})
+              -[:CONTAINS]->(:Commit)
+              -[:CONTAINS]->(f:FileRevision {hash: $fileHash})
+              -[:CONTAINS]->(cl:Clazz {name: $clazzName})
+            RETURN cl
+            LIMIT 1;
+            """,
+            Map.of(
+                "tokenId",
+                tokenId,
+                "repoName",
+                repoName,
+                "fileHash",
+                fileHash,
+                "clazzName",
+                clazzName)));
   }
 
-  public Optional<Clazz> findClassByLandscapeTokenAndRepositoryAndClazzFqn(final Session session,
-      final String tokenId, final String repoName, final String[] splitFqn, final String fileExt) {
+  public Optional<Clazz> findClassByLandscapeTokenAndRepositoryAndClazzFqn(
+      final Session session,
+      final String tokenId,
+      final String repoName,
+      final String[] splitFqn,
+      final String fileExt) {
     final String clazzName = splitFqn[splitFqn.length - 1];
     final String fileName = clazzName + fileExt;
     String[] fullFqn = new String[splitFqn.length + 1];
@@ -160,37 +200,54 @@ public class ClazzRepository {
     fullFqn[splitFqn.length - 1] = fileName;
     fullFqn[splitFqn.length] = clazzName;
 
-    return Optional.ofNullable(session.queryForObject(Clazz.class, """
-        MATCH (:Landscape {tokenId: $tokenId})
-          -[:CONTAINS]->(r:Repository {name: $repoName})
-          -[:CONTAINS]->(:Commit)
-          -[:CONTAINS]->(file:FileRevision {name: $fileName})
-          -[:CONTAINS]->(cl:Clazz {name: $clazzName})
-        MATCH (r)-[:HAS_ROOT]->(root:Directory {name: $repoName})
-        WITH root, $pathSegments AS pathSegments
-        MATCH p = (root)-[:CONTAINS]->(:Directory)
-                  -[:CONTAINS]->*(file)
-                  -[:CONTAINS]->(cl)
-        WHERE
-        all(j IN range(1, length(p)-1) WHERE nodes(p)[j].name = pathSegments[j-1])
-          AND size(nodes(p))-1 = size(pathSegments)
-        RETURN cl;
-        """, Map.of("tokenId", tokenId, "repoName", repoName, "clazzName", clazzName, "fileName",
-        fileName, "pathSegments", fullFqn)));
+    return Optional.ofNullable(
+        session.queryForObject(
+            Clazz.class,
+            """
+            MATCH (:Landscape {tokenId: $tokenId})
+              -[:CONTAINS]->(r:Repository {name: $repoName})
+              -[:CONTAINS]->(:Commit)
+              -[:CONTAINS]->(file:FileRevision {name: $fileName})
+              -[:CONTAINS]->(cl:Clazz {name: $clazzName})
+            MATCH (r)-[:HAS_ROOT]->(root:Directory {name: $repoName})
+            WITH root, $pathSegments AS pathSegments
+            MATCH p = (root)-[:CONTAINS]->(:Directory)
+                      -[:CONTAINS]->*(file)
+                      -[:CONTAINS]->(cl)
+            WHERE
+            all(j IN range(1, length(p)-1) WHERE nodes(p)[j].name = pathSegments[j-1])
+              AND size(nodes(p))-1 = size(pathSegments)
+            RETURN cl;
+            """,
+            Map.of(
+                "tokenId",
+                tokenId,
+                "repoName",
+                repoName,
+                "clazzName",
+                clazzName,
+                "fileName",
+                fileName,
+                "pathSegments",
+                fullFqn)));
   }
 
-  public Optional<Clazz> findClassFromInheritingClass(final Session session, final String tokenId,
-      final String repoName, final String clazzName) {
-    return Optional.ofNullable(session.queryForObject(Clazz.class, """
-        MATCH (:Landscape {tokenId: $tokenId})
-          -[:CONTAINS]->(:Repository {name: $repoName})
-          -[:CONTAINS]->(:Commit)
-          -[:CONTAINS]->(:FileRevision)
-          -[:CONTAINS]->(:Clazz)
-          -[:INHERITS]->(cl:Clazz {name: $clazzName})
-        RETURN cl
-        LIMIT 1;
-        """, Map.of("tokenId", tokenId, "repoName", repoName, "clazzName", clazzName)));
+  public Optional<Clazz> findClassFromInheritingClass(
+      final Session session, final String tokenId, final String repoName, final String clazzName) {
+    return Optional.ofNullable(
+        session.queryForObject(
+            Clazz.class,
+            """
+            MATCH (:Landscape {tokenId: $tokenId})
+              -[:CONTAINS]->(:Repository {name: $repoName})
+              -[:CONTAINS]->(:Commit)
+              -[:CONTAINS]->(:FileRevision)
+              -[:CONTAINS]->(:Clazz)
+              -[:INHERITS]->(cl:Clazz {name: $clazzName})
+            RETURN cl
+            LIMIT 1;
+            """,
+            Map.of("tokenId", tokenId, "repoName", repoName, "clazzName", clazzName)));
   }
 
   /**
@@ -203,12 +260,16 @@ public class ClazzRepository {
    *     Java).
    */
   public Map<String, Clazz> findStaticClassesWithFqnForApplicationAndCommitAndLandscapeToken(
-      final Session session, final String applicationName, final String commitHash,
+      final Session session,
+      final String applicationName,
+      final String commitHash,
       final String landscapeToken) {
 
     final Map<String, Clazz> filePathToClazzMap = new HashMap<>();
 
-    final Result result = session.query("""
+    final Result result =
+        session.query(
+            """
             MATCH (:Landscape {tokenId: $tokenId})
               -[:CONTAINS]->(:Repository)
               -[:HAS_ROOT]->(:Directory)
@@ -219,11 +280,15 @@ public class ClazzRepository {
               c AS clazz,
               reduce(fqn = nodes(p)[1].name, n IN nodes(p)[2..] | fqn + '/' + n.name) AS fqn;
             """,
-        Map.of("tokenId", landscapeToken, "appName", applicationName, "commitHash", commitHash));
+            Map.of(
+                "tokenId", landscapeToken, "appName", applicationName, "commitHash", commitHash));
 
-    result.queryResults().forEach(
-        queryResult -> filePathToClazzMap.put((String) queryResult.get("fqn"),
-            (Clazz) queryResult.get("clazz")));
+    result
+        .queryResults()
+        .forEach(
+            queryResult ->
+                filePathToClazzMap.put(
+                    (String) queryResult.get("fqn"), (Clazz) queryResult.get("clazz")));
 
     return filePathToClazzMap;
   }
