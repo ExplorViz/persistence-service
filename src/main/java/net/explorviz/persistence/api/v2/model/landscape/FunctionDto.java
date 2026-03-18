0043c2b3c7dfee572774133a2f34228e59609e25
Moritz Bugla <stu237520@mail.uni-kiel.de>
2026-03-18 21:42:14 +0100

    v2-API: Use function's database ID as method hash

--- src/main/java/net/explorviz/persistence/api/v2/model/landscape/FunctionDto.java
+++ src/main/java/net/explorviz/persistence/api/v2/model/landscape/FunctionDto.java
@@ -2,11 +2,8 @@ package net.explorviz.persistence.api.v2.model.landscape;
 
 import net.explorviz.persistence.ogm.Function;
 
-public record FunctionDto(
-    String name,
-    String methodHash
-) {
+public record FunctionDto(String name, String methodHash) {
   public FunctionDto(final Function ogmFunc) {
-    this(ogmFunc.getName(), ""); // TODO hash?
+    this(ogmFunc.getName(), ogmFunc.getId().toString());
   }
 }
