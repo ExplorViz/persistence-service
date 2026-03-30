54028b7104bec528d8c020cade3703407b58ff55
Moritz Bugla <stu237520@mail.uni-kiel.de>
2026-03-30 17:09:52 +0200

    Change v2-API landscape conversion to use Clazzes for buildings instead of FileRevisions

--- src/main/java/net/explorviz/persistence/api/v2/model/landscape/FunctionDto.java
+++ src/main/java/net/explorviz/persistence/api/v2/model/landscape/FunctionDto.java
@@ -2,6 +2,17 @@ package net.explorviz.persistence.api.v2.model.landscape;
 
 import net.explorviz.persistence.ogm.Function;
 
+/**
+ * Functions are not directly visualized in the software landscape; they can be viewed e.g. via
+ * popups, and they are central to the visualization of communication between classes.
+ *
+ * @param name Name of the function / method
+ * @param methodHash This is a value we used to calculate to match spans with the functions they
+ *     belong to, since multiple spans can refer to the same function. In the persistence-service,
+ *     we no longer need a hash calculation as we can simply return the ID of the function node.
+ *     Note that the naming "method" is for compatibility with the v2-API, which preferred the term
+ *     "method" over "function"
+ */
 public record FunctionDto(String name, String methodHash) {
   public FunctionDto(final Function ogmFunc) {
     this(ogmFunc.getName(), ogmFunc.getId().toString());
